fdca9c97ca9d4da4b8407095ecbcb3ba3c243361
JeoungB <eoehdektnv00@gmail.com>
2025-08-02 23:29:33 +0900

    헤더 작업

--- src/layouts/main-layout/Header.js
+++ src/layouts/main-layout/Header.js
@@ -9,11 +9,17 @@ const Header = () => {
         <img src={logo} alt="로고 이미지" className="w-20" />
         <p className="text-3xl font-bold text-gray-700 ">북픽</p>
       </div>
-      <input></input>
+      {/* 검색창 */}
+      {/* 검색 버튼 있어야할듯 핸드폰 생각하면 */}
+      <input
+        type="text"
+        placeholder="책 제목 또는 저자를 입력하세요."
+        className="w-1/2 h-10 bg-transparent border-2 border-black border-opacity-50 rounded px-5"
+      ></input>
       {/* 가입 */}
-      <div className="auth">
-        <button>회원가입</button>
-        <button>로그인</button>
+      <div className="auth w-48 h-full flex justify-between items-center text-l font-bold text-gray-600 ">
+        <button className="h-10 font-sans">회원가입</button>
+        <button className="h-10">로그인</button>
       </div>
     </header>
   );
