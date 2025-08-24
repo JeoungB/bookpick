8ef64c2e774c766cbe28c2dcbfb3313c6a9e2773
JeoungB <eoehdektnv00@gmail.com>
2025-08-24 20:32:24 +0900

    검색창 수정

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,9 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
   const searchRef = useRef(null);
+  const [hasText, setHasText] = useState("");
   const navigate = useNavigate();
 
   const searchHandler = (e) => {
@@ -12,17 +13,30 @@ const SearchBar = () => {
     navigate(`/search?keyword=${searchValue}`);
   };
 
+  const resetText = () => {
+    searchRef.current.value = "";
+    setHasText("");
+  }
+
+  const hasTextHandler = () => {
+    setHasText(searchRef.current.value);
+  };
+
   return (
     <form
       onSubmit={searchHandler}
-      className="search-bar w-full relative ml-56 border-2 border-black border-opacity-50 rounded flex max-lg:ml-44 max-md:w-3/5 max-md:ml-[180px] max-md:min-w-[290px] search-bar:hidden"
+      className="search-bar w-full relative ml-56 border-2 border-black border-opacity-50 rounded flex items-center max-lg:ml-44 max-md:w-3/5 max-md:ml-[180px] max-md:min-w-[290px] search-bar:hidden"
     >
       <input
         ref={searchRef}
+        onChange={hasTextHandler}
         type="text"
         placeholder="책 제목 또는 저자를 입력"
         className="w-5/6 h-10 bg-transparent px-5 focus:outline-none"
       ></input>
+      {hasText ? (
+        <button onClick={resetText} className="rounded-full absolute right-[80px] w-[35px] h-[35px] text-[1.2rem] hover:bg-gray-100">X</button>
+      ) : null}
       <button
         type="submit"
         className="w-[64.5px] h-10 font-pretendard font-semibold absolute right-0 border-l-2 border-gray-300 bg-white"
